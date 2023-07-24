832d4e700c6ded0c62a2e545e7831cdc06f47c1d
m-btt <dev@a.frings.space>
2023-07-24 21:49:59 +0200

    CSV comeback

--- overtime.py
+++ overtime.py
@@ -1,40 +1,33 @@
 import PySimpleGUI as sg
 import pandas as pd
  
+sg.theme("Dark Blue 3")
+
 # reading csv file
-df = pd.read_csv('samplecsv.csv', header=[0])
-print(df)
+df = pd.read_csv('samplecsv.csv', index_col='Start time')
 
 # what is what
-new_df = pd.DataFrame(['-START-'], ['-END-'])
-df.to_csv('samplecsv.csv', mode='a', header=True, index=False)
+test_df = pd.DataFrame(['-Startinput-'], ['-END-'])
+test_df.to_csv('samplecsv.csv', mode='a')
 
 # window layout
-layout = [[sg.Text("Enter start time:"), sg.Input(key='-START-', do_not_clear=True, size=(20, 1), enable_events=True), sg.CalendarButton('Current time')],
-          [sg.Text("Enter end time:"), sg.Input(key='-END-', do_not_clear=True, size=(20, 1), enable_events=True), sg.CalendarButton('Current time')],
+layout = [[sg.Text("Enter start time:"), sg.Input(readonly=True, do_not_clear=True, size=(20, 1), enable_events=True, key='-Startinput-'), sg.CalendarButton('Current start time', close_when_date_chosen=True, format='%H:%M:%S')],
+          [sg.Text("Enter end time:"), sg.Input(key='-END-', do_not_clear=True, size=(20, 1), enable_events=True), sg.CalendarButton('Current end time', format='%H:%M:%S')],
           [sg.Button('Submit Update'), sg.Exit()],
           ]
 
 window =sg.Window('Overtime logger',layout)
 
-def add_entry():
-    df = pd.concat([df, new_df], ignore_index=True)
-    return
-
-def save_to_csv():
-# save to csv
-    new_df.to_csv('samplecsv.csv', encoding='UTF-8', header=None, index=False, mode='a')
+def new_entry():
+    new_df = pd.concat([new_df, df.ix[:]])
 
 # button stuff
 while True:
     event, values = window.read()
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
-    if event == 'Submit Update':
-       add_entry
-       print(new_df)
+    elif event == 'Submit Update':
+       new_entry
        sg.popup("Update submitted!")
-    elif event == 'Calendar':
-        window['_CALENDAR_'](disabled=True)
 
 window.close()  
