081fa9c4fc09daf7f31bca754ed5fe240d2eebb7
m-btt <dev@a.frings.space>
2023-07-25 00:04:53 +0200

    It's alive!

--- overtime.py
+++ overtime.py
@@ -1,21 +1,25 @@
 import PySimpleGUI as sg
 import pandas as pd
- 
+import datetime as dt
+
 sg.theme("Dark Blue 3")
 
 # reading csv file
 df = pd.read_csv('samplecsv.csv', index_col=[-1])
 
 # what is what
-new_df = pd.DataFrame(['-Startinput-'], ['-END-'])
+rightnow = pd.Timestamp('now')
+daytoday = pd.Timestamp.today()
+new_df = pd.DataFrame([rightnow], [daytoday])
 
+def addupdate():
+     df = pd.concat([df, new_df], ignore_index=True, ignore_header=False)
+  
 # window layout
-layout = [[sg.Text("Enter start time:"), sg.Input(readonly=True, do_not_clear=True, size=(20, 1), enable_events=True, key='-Startinput-'), sg.CalendarButton('Current start time', close_when_date_chosen=True, format='%H:%M:%S')],
-          [sg.Text("Enter end time:"), sg.Input(readonly=True, key='-END-', do_not_clear=True, size=(20, 1), enable_events=True), sg.CalendarButton('Current end time', close_when_date_chosen=True, format='%H:%M:%S')],
-          [sg.Button('Submit Update'), sg.Exit()],
+layout = [[sg.Button('Submit Update'), sg.Exit()],
           ]
 
-window =sg.Window('Overtime logger',layout)
+window =sg.Window('Overtime logger',layout,size=(180,180))
    
 # button stuff
 while True:
@@ -23,7 +27,8 @@ while True:
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
     elif event == 'Submit Update':
-       new_df.to_csv('samplecsv.csv', mode='a', index=[-1], header=False)
+       addupdate
+       new_df.to_csv('samplecsv.csv', mode='a', index=True, header=False)
        sg.popup("Update submitted!")
 
 window.close()  
